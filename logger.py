078910ec48c97a21ce73f56d5bc4d63d37aa8cd3
btuan <brian.tuan@gmail.com>
2017-05-30 00:02:41 -0500

    Initial commit. Stand-up logging server.

---
+++ logger.py
@@ -0,0 +1,57 @@
+"""
+
+A simple HTTP request logger to log messages sent by the instrumentation.
+
+Author: Brian Tuan
+
+Last Modified: May 29, 2017
+
+"""
+
+import click
+import json
+import time
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http import HTTPStatus as HTTP
+
+
+def process_POST(data):
+    if 'HIT_ID' not in data or 'action' not in data:
+        return
+
+    t_received = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
+    with open("{}.log".format(data['HIT_ID']), 'a') as f:
+        f.write("{}\n{}\n\n".format(t_received, data['action']))
+
+
+class InstrumentationHandler(SimpleHTTPRequestHandler):
+    def respond(self):
+        self.send_response(HTTP.OK)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        print(self.path)
+        self.respond()
+
+    def do_POST(self):
+        self.respond()
+        data = json.loads(self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8'))
+        process_POST(data)
+
+
+@click.command()
+@click.option("-v", "--verbose", default=True, is_flag=True, help="Toggle for verbosity.")
+def run(verbose):
+    server_address = ('localhost', 8000)
+    httpd = HTTPServer(server_address, InstrumentationHandler)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
+
+
+
+
